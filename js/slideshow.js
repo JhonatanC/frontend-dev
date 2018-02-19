0ae394a221d04a6ef047c803881b04b05a324228
JhonatanC <jhonatanchurtado@outlook.com>
2018-02-19 17:19:23 -0500

    Mostrando el flujo del comportamiento de columnas, falta agregar las clases order

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,28 +1,66 @@
+// Variables
+var $prev = $('#prev');
+var $next = $('#next');
+var $dot = $('#dot');
+var $dotClass = $('.dot');
+var $mySlides = $('.mySlides');
+var $templateDoc = `<span class="dot"></span>`;
+
+
+// Thumbnail controls dinamics
+$mySlides.each(function(ev){
+  $dot.append($templateDoc)
+})
+
+// Next/previous controls
+$prev.click(function(){
+  showSlides(slideIndex += -1)
+})
+
+$next.click(function(){
+  showSlides(slideIndex += 1);
+})
+
+/*
+<span class="dot" onclick="currentSlide(1)"></span> 
+<span class="dot" onclick="currentSlide(2)"></span> 
+<span class="dot" onclick="currentSlide(3)"></span>
+*/
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
 function showSlides(n) {
+
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
+
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none"; 
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
+
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
+
 }
+
+
+
+
+
+// Reproducción automática
+/*setInterval(function(){
+  slideIndex++
+  showSlides(slideIndex);
+}, 7000);*/
