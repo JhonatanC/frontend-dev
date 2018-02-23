7d7049aed9ff647a5e0b872f06d4203e8cf6b723
JhonatanC <jhonatanchurtado@outlook.com>
2018-02-23 11:33:59 -0500

    Añadiendo funcionalidad del menú de navegación

--- js/nav-mobile.js
+++ js/nav-mobile.js
@@ -1,18 +1,52 @@
 $(function(){
 
-	var $navButton = $('#nav-button')
-	var $menu = $('#menu')
+    // Botón de navegación para móviles
+    var $btnNav = $('#btn-nav');
+    var $showMenu = $('nav').find('.desktop-menu');
 
-	$navButton.click((ev) => {
-		$menu.slideToggle()
-	})
+    $btnNav.click(function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $showMenu.toggleClass('show');
+    });
 
-	var $down = $('.down')
+    // Enlaces menú principal
+    var $item = $('.menu li');
+    var $subItem = $('.menu li ul');
 
-	$down.click(function(ev){
-		//$(this).children('ul').slideDown()
-		$('.down ul').not($('.down ul',this)).slideUp('slow').next();
-		$(this).children('ul').slideToggle('slow').end();
-	})
+    var $subMenu = $('ul .sub-menu');
+    var $subMenuN = $('.sub-menu ul');
+
+    // Enlaces sub menú
+    $item.click(function(e){
+
+        var $class = $(this).attr('class');
+
+        if($class === 'sub-menu'){
+            $($subMenuN).not($('ul',this)).slideUp('slow').next();
+            $(this).children('ul').slideToggle('slow').end();
+        } else {
+            $($subItem).not($('ul',this)).slideUp('slow').next();
+            $(this).children('ul').slideToggle('slow').end();
+        }
+
+        e.preventDefault();
+        e.stopPropagation();
+
+    });
+
+    // Obtener la url del valor seleccionado y redireccionar
+    var go = $('nav a');
+
+    go.click(function(e){
+
+        var $url = $(this).attr('href');
+        
+        if( $url == '#' ){
+            e.preventDefault();
+        } else {
+            window.location.href = $url;
+        }
+    });
 
 });
